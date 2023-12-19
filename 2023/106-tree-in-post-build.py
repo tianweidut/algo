fb33368c9719b54780ac85cd54b6464f8fedc57e
tianwei <liutianweidlut@gmail.com>
2023-12-19 22:29:22 +0800

    add 106: build tree from inorder and postorder

---
+++ 2023/106-tree-in-post-build.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        if not inorder or not postorder:
+            return None
+
+        val = postorder[-1]
+        idx = inorder.index(val)
+
+        return TreeNode(
+            val=val,
+            left=self.buildTree(inorder[:idx], postorder[0:idx]),
+            right=self.buildTree(inorder[idx+1:], postorder[idx:-1]),
+        )
