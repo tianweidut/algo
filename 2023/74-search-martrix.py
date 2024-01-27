07a0059bf94125a8de942c9c78f66e358e3f5666
tianwei <liutianweidlut@gmail.com>
2024-01-27 23:01:39 +0800

    add 74: search matrix

---
+++ 2023/74-search-martrix.py
@@ -0,0 +1,32 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+
+        def search(nums):
+            l, r = 0, len(nums) - 1
+
+            while l <= r:
+                mid = l + (r - l) // 2
+                if target == nums[mid]:
+                    return True
+                elif target < nums[mid]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return False
+
+        i = 0
+        while i < m:
+            val = matrix[i][n-1]
+            if val == target:
+                return True
+            elif val > target:
+                return search(matrix[i])
+            else:
+                i += 1
+
+        return False
+            
