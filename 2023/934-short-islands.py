3359729200e2fb4200ab4b2d93298aa4dacda484
tianwei <liutianweidlut@gmail.com>
2024-01-31 16:17:41 +0800

    update

--- 2023/934-short-islands.py
+++ 2023/934-short-islands.py
@@ -1,27 +1,23 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
+        from collections import deque
         if not grid or not grid[0]:
             return -1
 
         self.directions = [-1, 0, 1, 0, -1]
         self.m, self.n = len(grid), len(grid[0])
-        self.sides_queue = []
+        self.sides_queue = deque()
 
         def dfs(i, j):
-            if i < 0 or i >= self.m or j < 0 or j >= self.n:
-                return
-
             if grid[i][j] == 0:
                 self.sides_queue.append((i, j))
-                return
-
-            if grid[i][j] == 2:
                 return
 
             grid[i][j] = 2
             for d in range(0, len(self.directions) - 1):
                 di, dj = i + self.directions[d], j + self.directions[d + 1]
-                dfs(di, dj)
+                if 0 <= di < self.m and 0 <= dj < self.n and grid[di][dj] != 2: 
+                    dfs(di, dj)
 
         found = False
         for i in range(0, self.m):
@@ -36,7 +32,7 @@ class Solution:
         while self.sides_queue:
             levels += 1
             for _ in range(len(self.sides_queue)):
-                i, j = self.sides_queue.pop(0)
+                i, j = self.sides_queue.popleft()
                 grid[i][j] = 2
 
                 for d in range(0, len(self.directions) - 1):
